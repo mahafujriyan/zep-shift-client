aee4775fa42e72324e6c1f9b8be300d25231b6de
riy8n <mahafujhossainriyan02@gmail.com>
2025-07-14 02:41:43 +0600

    Added the bann update

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='my-4 p-2'>
+        <div className='my-4 p-3'>
             <img src="/Assets/banner/banner1.png" alt="" />
         </div>
     );
